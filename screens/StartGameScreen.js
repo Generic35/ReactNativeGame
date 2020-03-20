f16b3ade37bafe7adc942d63c295139514022413
Eric Lamontagne <eric.lamontagne@mphasis.com>
2020-03-20 17:13:15 -0400

    (feature) Add a 'Start a game' screen

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
+
+const StartGameScreen = props => {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Start a new game</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select a number</Text>
+        <TextInput></TextInput>
+        <View style={styles.buttonsContainer}>
+          <Button title="Reset"></Button>
+          <Button title="Confirm"></Button>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center'
+  }
+});
+export default StartGameScreen;
