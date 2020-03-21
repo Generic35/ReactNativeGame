64dbbcae82d3475ef46a96240b4e0c088f965e26
Eric Lamontagne <eric.lamontagne@mphasis.com>
2020-03-21 13:20:35 -0400

    (feature) Forward props to inner component. Configure the input

--- components/Input.js
+++ components/Input.js
@@ -2,7 +2,17 @@ import React from 'react';
 import { TextInput, StyleSheet } from 'react-native';
 
 const Input = props => {
-  return <TextInput style={{ ...styles.input, ...props.style }} />;
+  return (
+    <TextInput
+      {...props}
+      style={{ ...styles.input, ...props.style }}
+      blurOnSubmit
+      autoCapitalize="none"
+      autoCorrect={false}
+      keyboardType="number-pad"
+      maxLength={2}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
